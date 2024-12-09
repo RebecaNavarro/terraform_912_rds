2c00a877dca67c810f7c5502baf98fc895f210b5
RebecaNavarro <130471557+RebecaNavarro@users.noreply.github.com>
2024-12-09 08:53:35 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "db-name" {
+ description = "Set the database name"
+ type = string
+}
+
+
+variable "db-username" {
+ default = "Set the username for db"
+ type = string
+}
+
+
+variable "db-password" {
+ default = "Set the username for db"
+ type = string
+}
